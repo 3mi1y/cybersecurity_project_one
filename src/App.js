ef65e6a5c558988c01eaf65693c4a132db8c58b0
jordan.purinton <jordanomac@gmail.com>
2018-04-09 12:58:25 -0600

    Add styled components.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
-import './App.css';
+import styled from 'styled-components';
+
+const Header = styled.header`
+background-color: #222;
+padding: 40px;
+color: white;
+text-align: center;
+`
+
+const Intro = styled.p`
+font-size: large;
+text-align: center;  
+`
 
 class App extends Component {
   render() {
     return (
       <div>
-        <header className="app-header">
+        <Header>
           <h1>CyberSecurity Project 1</h1>
-        </header>
-        <p className="app-intro">
-          GREAT STUFF WILL GO HERE
-        </p>
+        </Header>
+        <Intro>
+          Enter some data why don't ya?
+        </Intro>
       </div>
     );
   }
