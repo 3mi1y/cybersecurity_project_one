9cbd8b84fd0935af12c1e76ec43d59cb5fba2e42
Emily <emily@portal.org>
2018-04-10 21:26:31 -0700

    made some more changes

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ require('dotenv').config()
 app.use(express.static(path.join(__dirname, "client", "build")))
 
 const bodyParser = require('body-parser')
-//app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }))
 const jsonParser = bodyParser.json()
 
 // cross origin things
