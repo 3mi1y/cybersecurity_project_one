224dd99ed2deb61696d144020fff3501301e3cf6
Emily <emily@portal.org>
2018-04-10 21:45:23 -0700

    Resolve merge conflicts

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
+import TextField from 'material-ui/TextField';
+import Button from 'material-ui/Button';
 
 const Header = styled.header`
 background-color: #222;
@@ -13,7 +15,25 @@ font-size: large;
 text-align: center;  
 `
 
+const TextContainer = styled.div`
+text-align: center;
+justify-content: center;
+`
+
+const buttonStyles = {
+  marginTop: '24px'
+}
+
 class App extends Component {
+  state = {
+    name: '',
+  };
+  handleChange = name => event => {
+    this.setState({
+      [name]: event.target.value,
+    });
+  };
+
   render() {
     return (
       <div>
@@ -23,9 +43,19 @@ class App extends Component {
         <Intro>
           Enter some data why don't ya? 🤗
         </Intro>
+        <TextContainer>
+          <TextField
+            id="name"
+            label="Name"
+            value={this.state.name}
+            onChange={this.handleChange('name')}
+          /><br/>
+          <Button variant="raised" color="primary" style={buttonStyles}>
+            Submit
+          </Button>
+        </TextContainer>
       </div>
     );
   }
 }
-
 export default App;
